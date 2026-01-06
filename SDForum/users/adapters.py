c3da43fe97971ce294dc6eb279673be02d570784
Tanish431 <tanishsoni431@gmail.com>
2026-01-06 21:32:24 +0530

    small email fix

--- SDForum/users/adapters.py
+++ SDForum/users/adapters.py
@@ -5,5 +5,5 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         email = sociallogin.user.email
 
-        if not email or not email.endswith("f@pilani.bits-pilani.ac.in"):
+        if not email or not email.endswith("@pilani.bits-pilani.ac.in"):
             raise PermissionDenied("Access restricted to BITS Pilani email accounts.")
